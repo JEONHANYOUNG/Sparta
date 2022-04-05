ff55b9c4a1501814a75975706b0b1a1cab4ce898
JEONHANYOUNG <91587402+JEONHANYOUNG@users.noreply.github.com>
2022-04-05 23:39:15 +0900

    3주차 파이썬 배우기

--- pythonprac/hello.py
+++ pythonprac/hello.py
@@ -94,25 +94,3 @@
 
 # print(get_age('bob'))    <= 20
 # print(get_age('kay'))    <= 해당하는 이름이 없습니다
-
-
-# 예제 문제
-# import requests # requests 라이브러리 설치 필요
-
-# r = requests.get('http://spartacodingclub.shop/sparta_api/seoulair')
-# rjson = r.json()
-
-# rows = rjson['RealtomeCityAir']['row']
-
-# for row in rows:
-    # gu_name = row['MSRSTE_NM']   <= gu_name은 row의 'MSRSTE_NM'
-    # gu_mise = row['IDEX_MVL']    <= gu_mise는 row의 'IDEX_MVL'
-
-    # 1. 지역 구와 미세먼지 두개 항목에 대해 값 출력
-    # print(gu_name,gu_mise)
-
-    # 2. 미세먼지가 40보다 작으면 값 출력
-    # if gu_mise < 40:
-        # print(gu_name)
-
-# print(rjson)
