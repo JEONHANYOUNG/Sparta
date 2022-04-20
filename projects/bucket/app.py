b3fe2a2c67853c078815c6c316b1367391b4c489
JEONHANYOUNG <91587402+JEONHANYOUNG@users.noreply.github.com>
2022-04-20 23:03:03 +0900

    4주차 화성땅 공동구매 - GET 방식 연습(주문 저장)

--- projects/bucket/app.py
+++ projects/bucket/app.py
@@ -34,7 +34,8 @@ def bucket_done():
 
 @app.route("/bucket", methods=["GET"])
 def bucket_get():
-    return jsonify({'msg': 'GET 연결 완료!'})
+    bucket_list = list(db.users.find({}, {'_id': False}))
+    return jsonify({'buckets': 'bucket_list 완료!'})
 
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)
